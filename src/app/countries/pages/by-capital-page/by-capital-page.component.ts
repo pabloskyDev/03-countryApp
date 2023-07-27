f15683c7dd28b7ecd44fcc2f973d4e1d0296d0ca
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2023-07-26 22:59:14 -0500

    [UPD] Use component table countries

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country.dto';
 
 @Component({
   selector: 'country-by-capital-page',
@@ -7,8 +9,14 @@ import { Component } from '@angular/core';
   ]
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = [];
+  constructor(
+    private countryService: CountriesService
+  ) {}
 
   searchByCapital(term: string): void {
-    console.log({term});
+    this.countryService.searchCapital(term).subscribe(countries => {
+      this.countries = countries;
+    })
   }
 }
