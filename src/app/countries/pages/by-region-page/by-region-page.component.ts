904679b1d763b044f406272283a147f11acb9b6e
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2023-08-21 22:17:43 -0500

    [UPD] Set default array regions

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.dto';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'country-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,12 +13,15 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  selectedRegion!: Region;
 
   constructor(
     private countryService: CountriesService
   ) {}
 
-  searchByRegion(term: string): void {
+  searchByRegion(term: Region): void {
+    this.selectedRegion = term;
     this.isLoading = true;
     this.countryService.searchRegion(term).subscribe(countries => {
       this.isLoading = false;
